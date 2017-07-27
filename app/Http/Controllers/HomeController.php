3bac5c52a1c1af73390e28740b625e9eaca6a8c4
Napaporn <pns.playgirl@gmail.com>
2017-07-27 16:48:40 +0700

    แก้ไข

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ class HomeController extends Controller
 {
     /** 
      * Create a new controller instance.
-     *
+     *   
      * @return void
      */
     public function __construct()
@@ -24,6 +24,6 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
-        
+
     }
 }
