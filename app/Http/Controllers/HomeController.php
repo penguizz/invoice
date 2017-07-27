c4bc6e4972ce8a47935793f1c4ad8328ff5de108
Napaporn <pns.playgirl@gmail.com>
2017-07-27 16:38:06 +0700

    cccc

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
+    /** 
      * Create a new controller instance.
      *
      * @return void
@@ -24,5 +24,6 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+        
     }
 }
